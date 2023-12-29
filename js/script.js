b443c7b9dc0f446cbfd62b4e91ba5175d985f36c
jonhvito <victorjoao8817@gmail.com>
2023-12-29 16:35:29 -0300

    refactor and updated code

--- js/script.js
+++ js/script.js
@@ -1,100 +1,99 @@
-/*====================================== Typing Animation ================================*/ 
-var typed = new Typed(".typing", {
-  strings: ["","Web Designer","web Developer","Graphic Designer","YouTube"],
-  typeSpeed:100,
-  BackSpeed:60,
-  loop:true
+/*====================================== Typing Animation ================================*/
+const typingStrings = [
+  "",
+  "Web Designer",
+  "Web Developer",
+  "Graphic Designer",
+  "YouTube",
+]
+const typeSpeed = 100
+const backSpeed = 60
+const loop = true
+
+const typed = new Typed(".typing", {
+  strings: typingStrings,
+  typeSpeed: typeSpeed,
+  backSpeed: backSpeed,
+  loop: loop,
 })
 
-/*================================================ Aside =============================================*/ 
+/*================================================ Aside =============================================*/
 
-const nav = document.querySelector(".nav"),
-    navList = nav.querySelectorAll("li"),
-    totalNavList = navList.length,
-    allSection = document.querySelectorAll(".section"),
-    totalSection = allSection.length;
-    for(let i=0; i<totalNavList; i++)
-    {
-      const a = navList[i].querySelector("a");
-      a.addEventListener("click", function()
-      {
-        removeBackSection();
-        for(let j=0; j<totalNavList; j++)
-        {
-          if(navList[j].querySelector("a").classList.contains("active"))
-          {
-            addBackSection(j);
-            //allSection[j].classList.add("back-section");
-          }
-          navList[j].querySelector("a").classList.remove("active");
-        }
-        this.classList.add("active")
-        showSection(this);
-        if(window.innerWidth < 1200)
-        {
-          asideSectionTogglerbtn();
-        }
-      })
+const nav = document.querySelector(".nav")
+const navList = nav.querySelectorAll("li")
+const allSection = document.querySelectorAll(".section")
+const totalSection = allSection.length
 
+function removeBackSection() {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("back-section")
+  }
+}
+
+function addBackSection(num) {
+  allSection[num].classList.add("back-section")
+}
+
+function showSection(element) {
+  allSection.forEach((section) => section.classList.remove("active", "out"))
+  const target = element.getAttribute("href").split("#")[1]
+  document.querySelector("#" + target).classList.add("active")
+  setTimeout(() => {
+    document.querySelector("#" + target).classList.remove("out")
+  }, 800)
+}
+
+function updateNav(element) {
+  navList.forEach((item) => {
+    item.querySelector("a").classList.remove("active")
+    const target = element.getAttribute("href").split("#")[1]
+    if (target === item.querySelector("a").getAttribute("href").split("#")[1]) {
+      item.querySelector("a").classList.add("active")
     }
+  })
+}
 
-    function removeBackSection()
-    {
-      for(let i=0; i<totalSection; i++)
-      {
-        allSection[i].classList.remove("back-section");
-      }
-    }
+function handleNavLinkClick(event) {
+  event.preventDefault()
+  removeBackSection()
 
-    function addBackSection(num)
-    {
-      allSection[num].classList.add("back-section")
-    }
+  navList.forEach((item) => item.querySelector("a").classList.remove("active"))
+  this.classList.add("active")
+  showSection(this)
 
-    function showSection(element)
-    {
-      for(let i=0; i<totalSection; i++)
-      {
-        allSection[i].classList.remove("active");
-      }
-      const target = element.getAttribute("href").split("#")[1];
-      document.querySelector("#" + target).classList.add("active")
-    }
+  if (window.innerWidth < 1200) {
+    asideSectionTogglerbtn()
+  }
+}
 
-    function updateNav(element)
-    {
-      for(let i=0; i<totalNavList; i++)
-      {
-        navList[i].querySelector("a").classList.remove("active");
-        const target = element.getAttribute("href").split("#")[1];
-        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
-        {
-          navList[i].querySelector("a").classList.add("active");
-        }
-      }
-    }
-    document.querySelector(".hire-me").addEventListener("click", function()
-    {
-       const sectionIndex = this.getAttribute("data-section-index");
-       //console.log(sectionIndex);
-       showSection(this);
-       updateNav(this); 
-       removeBackSection();
-       addBackSection(sectionIndex);
-    })
-    
-    const navTogglerBtn = document.querySelector(".nav-toggler"),
-        aside = document.querySelector(".aside");
-        navTogglerBtn.addEventListener("click", () => 
-        {
-          asideSectionTogglerbtn();
-        })
-        function asideSectionTogglerbtn()
-        {
-          aside.classList.toggle("open");
-          navTogglerBtn.classList.toggle("open");
-          for(let i=0;i<totalSection; i++ )
-          {
-            allSection[i].classList.toggle("open")
-          }
-        }
+navList.forEach((item) => {
+  const a = item.querySelector("a")
+  a.addEventListener("click", handleNavLinkClick)
+})
+
+document.querySelector(".hire-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index")
+  showSection(this)
+  updateNav(this)
+  removeBackSection()
+  addBackSection(sectionIndex)
+
+  if (window.innerWidth >= 1200) {
+    asideSectionTogglerbtn()
+  }
+})
+
+const navTogglerBtn = document.querySelector(".nav-toggler")
+const aside = document.querySelector(".aside")
+
+navTogglerBtn.addEventListener("click", () => {
+  aside.classList.toggle("open")
+  navTogglerBtn.classList.toggle("open")
+  allSection.forEach((section) => section.classList.toggle("open"))
+})
+
+function asideSectionTogglerbtn() {
+  aside.classList.toggle("open")
+  navTogglerBtn.classList.toggle("open")
+  allSection.forEach((section) => section.classList.toggle("open"))
+}
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,45 +1,53 @@
-/* ===================================== Toggle Style Switcher =================================*/ 
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () => {
-  document.querySelector(".style-switcher").classList.toggle("open");
-})
-// hide style - switcher on scrol
-window.addEventListener("scroll", () =>  {
-  if(document.querySelector(".style-switcher").classList.contains("open"))
-  {
-    document.querySelector(".style-switcher").classList.remove("open");
+/* ===================================== Toggle Style Switcher =================================*/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler")
+
+styleSwitcherToggle.addEventListener("click", toggleStyleSwitcher)
+
+function toggleStyleSwitcher() {
+  document.querySelector(".style-switcher").classList.toggle("open")
+}
+
+// hide style - switcher on scroll
+window.addEventListener("scroll", hideStyleSwitcherOnScroll)
+
+function hideStyleSwitcherOnScroll() {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open")
   }
-})
+}
+
 /* ===================================== Theme Colors =================================*/ 
 const alternateStyles = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color) 
-{
+
+function setActiveStyle(color) {
   alternateStyles.forEach((style) => {
-    if(color === style.getAttribute("title")) 
-    {
+    if(color === style.getAttribute("title")) {
       style.removeAttribute("disabled")
-    } 
-    else 
-    {
+      // Fecha o seletor de estilo quando uma cor for selecionada
+      document.querySelector(".style-switcher").classList.remove("open");
+    } else {
       style.setAttribute("disabled", "true");
     }
   })
 }
-/* ===================================== Theme Light An Dark Mode =================================*/ 
-const daynNight = document.querySelector(".day-night");
-daynNight.addEventListener("click", () => {
-  daynNight.querySelector("i").classList.toggle("fa-sun");
-  daynNight.querySelector("i").classList.toggle("fa-moon");
-  document.body.classList.toggle("dark");
-})
-window.addEventListener("load",() => {
-  if(document.body.classList.contains("dark"))
-  {
-    daynNight.querySelector("i").classList.add("fa-sun");
-  }
-  else
-  {
-    daynNight.querySelector("i").classList.add("fa-moon");
-  }
-})
 
+/* ===================================== Theme Light An Dark Mode =================================*/
+const daynNight = document.querySelector(".day-night")
+
+daynNight.addEventListener("click", toggleDayNight)
+
+function toggleDayNight() {
+  daynNight.querySelector("i").classList.toggle("fa-sun")
+  daynNight.querySelector("i").classList.toggle("fa-moon")
+  document.body.classList.toggle("dark")
+}
+
+window.addEventListener("load", setInitialThemeIcon)
+
+function setInitialThemeIcon() {
+  if (document.body.classList.contains("dark")) {
+    daynNight.querySelector("i").classList.add("fa-sun")
+  } else {
+    daynNight.querySelector("i").classList.add("fa-moon")
+  }
+}
